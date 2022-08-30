feb867e6c0c78b32682376c002c698527e44eb6d
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-08-29 17:34:59 -0700

    more types

--- Types/variables.ts
+++ Types/variables.ts
@@ -1,3 +1,12 @@
 let movieTitle: string = "Bullet Train";
 movieTitle = "The Man from Uncle";
 //movieTitle = 9 This gives me a big old error if I try to make it not a string
+
+let TVShow = "Peaky Blinders"
+TVShow = 8 // gives the same error, meaning typescript actually automattically types things for you you dont have to explicitly write it
+
+//you can type things to any though and it will give no errors
+
+let thing: any = "hi";
+thing = 9;
+thing();
