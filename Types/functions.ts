11aad02fec5b69cab37d59032f2cde30e0727393
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-08-31 13:22:26 -0700

    defualt values

--- Types/functions.ts
+++ Types/functions.ts
@@ -7,3 +7,13 @@ function square(num: number){
 function greek(food: string){
   return 'olives';
 }
+
+const friendPotential = (person: string, age: number, isFunny: boolean) => {}
+
+friendPotential("Zuma", 1.5, true) // this works ts will give you exact errors for arguemnts missing tho which is cool.
+
+//providing default values must come after annotation
+
+function whatsForLunch(food: string = 'Burrito'){
+  return `We get to have a ${food} for lunch.`
+}
