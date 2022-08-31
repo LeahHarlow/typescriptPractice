f4958c8c7f9624706c84ec9fbaa2c0917c19e2cd
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-08-31 13:27:46 -0700

    typing returned values

--- Types/functions.ts
+++ Types/functions.ts
@@ -1,19 +1,26 @@
 //annotating functions
 
-function square(num: number){
+function square(num: number) {
   return num * num;
 }
 
-function greek(food: string){
+function greek(food: string) {
   return 'olives';
 }
 
-const friendPotential = (person: string, age: number, isFunny: boolean) => {}
+const friendPotential = (person: string, age: number, isFunny: boolean) => {};
 
-friendPotential("Zuma", 1.5, true) // this works ts will give you exact errors for arguemnts missing tho which is cool.
+friendPotential('Zuma', 1.5, true); // this works ts will give you exact errors for arguemnts missing tho which is cool.
 
 //providing default values must come after annotation
 
-function whatsForLunch(food: string = 'Burrito'){
-  return `We get to have a ${food} for lunch.`
+function whatsForLunch(food: string = 'Burrito') {
+  return `We get to have a ${food} for lunch.`;
+}
+
+// if you forget to include a return in a function ts wont give you an error it will just say void.
+// unless you also type the returned value ie
+
+function add(num: number): number {
+  return num + num;
 }
