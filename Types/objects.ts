c63ee32aa54a57a568e0ac798660c0f93f40927a
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-09-08 21:20:50 -0700

    inline vs variable arguements

--- Types/objects.ts
+++ Types/objects.ts
@@ -8,3 +8,17 @@ printName({ first: 'Lola', last: 'Dobson' });
 //weird example but its possible and highlights the different types of brackets
 //the first set of brackets is the typed input object and the second set is the actual input object.
 let coordinate: {x: number, y: number} = {x:10, y:13}
+
+
+// this is also super weird but it shows that you can type the expected return from a function to be an object literal as well.
+function randoTinyCoordinate(): {x: number, y: number} {
+  return {x: Math.random(), y: Math.random()}
+}
+
+// whoever was the brainchild of ts decided that if you pass objects directly into a function if you add any extra arguements from when you typed the functions expected input then you will get an error, BUT if you pass in a variable with extra info you wont get an error bc the function will just scan for what it needs, also you can pass in less arguements apperantly too
+
+printName({first:"Mick", last: "Jagger", age: 473});
+
+const singer = {first:"Mick", last: "Jagger", age: 473};
+
+printName(singer);
