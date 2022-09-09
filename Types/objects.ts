bc5b8b72d0a792dca6b5967045272915fd1c0808
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-09-08 20:44:10 -0700

    bracket difff

--- Types/objects.ts
+++ Types/objects.ts
@@ -2,4 +2,9 @@ function printName(person: { first: string; last: string }): void {
   console.log(`${person.first}, ${person.last}`);
 }
 
-printName({ first: 'lola', last: 'Dobson' });
+printName({ first: 'Lola', last: 'Dobson' });
+
+
+//weird example but its possible and highlights the different types of brackets
+//the first set of brackets is the typed input object and the second set is the actual input object.
+let coordinate: {x: number, y: number} = {x:10, y:13}
