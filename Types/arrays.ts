8366d21abb35bb58fcfe5623af700d6e3e40e4d7
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-09-16 19:31:05 -0600

    nested array syntax

--- Types/arrays.ts
+++ Types/arrays.ts
@@ -21,3 +21,17 @@ type Point = {
 const coordinates: Point[] = [];
 
 coordinates.push({ x: 3, y: 2 });
+
+// ok so maybe you wanted a nested array though, you need to add that many arrays to the end of your typing etc
+
+const Board: string[][] = [
+  ['_', '_', '_'],
+  ['_', '_', '_'],
+  ['_', '_', '_'],
+];
+
+let Plays: string[][] = [
+  ['X', 'O', 'X'],
+  ['O', 'O', 'X'],
+  ['_', '_', 'X'],
+];
