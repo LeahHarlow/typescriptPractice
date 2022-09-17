3c7c88b987d82a4e9f63500e820e1616e20dead9
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-09-16 19:36:19 -0600

    part 2 done

--- Types/arraysEx.ts
+++ Types/arraysEx.ts
@@ -11,6 +11,8 @@ let ages: number[] = [];
 // Create an array variable called gameBoard that starts as an empty array.
 // It should be typed to hold a 2 dimensional array of strings
 
+let gameBoard: string[][] = [];
+
 // **********************************************
 // ******************* PART 3 *******************
 // **********************************************
