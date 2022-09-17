1fae6af86f754f03fd04ef686de158020fe71be2
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-09-17 15:26:10 -0600

    proof it works

--- Types/unionTypes.ts
+++ Types/unionTypes.ts
@@ -2,20 +2,24 @@
 
 //let user: string | number = "Cabo"
 
-const printUser = (user: string | number):void => {
-  console.log(`The user is called ${user}`)
-}
+const printUser = (user: string | number): void => {
+  console.log(`The user is called ${user}`);
+};
 
 //both of these are totally cool now
-printUser("Cabo");
-printUser(247)
+printUser('Cabo');
+printUser(247);
 
 // we might want to do some type narrowing though once we do get an input
 
 // typescript is so smart that hovering over price on line 17 || 18 we get both types and on line 20 its only number type
 const calculateTax = (price: number | string, tax: number): number => {
-  if(typeof price === 'string'){
-    price = parseFloat(price.replace("$", ""))
+  if (typeof price === 'string') {
+    price = parseFloat(price.replace('$', ''));
   }
   return price * tax;
-}
+};
+
+//pudding
+calculateTax(45, .03)
+calculateTax("$45", .03)
