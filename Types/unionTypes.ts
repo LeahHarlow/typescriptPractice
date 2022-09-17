ee88fe50e8714457bf38561f7d9056b061fad083
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-09-17 15:24:04 -0600

    adding conditionals for type narrowing within functions

--- Types/unionTypes.ts
+++ Types/unionTypes.ts
@@ -1,5 +1,21 @@
 // you can use a single | to join types if you dont know what a type will be at runtime.
 
-let user: string | number;
+//let user: string | number = "Cabo"
 
+const printUser = (user: string | number):void => {
+  console.log(`The user is called ${user}`)
+}
 
+//both of these are totally cool now
+printUser("Cabo");
+printUser(247)
+
+// we might want to do some type narrowing though once we do get an input
+
+// typescript is so smart that hovering over price on line 17 || 18 we get both types and on line 20 its only number type
+const calculateTax = (price: number | string, tax: number): number => {
+  if(typeof price === 'string'){
+    price = parseFloat(price.replace("$", ""))
+  }
+  return price * tax;
+}
