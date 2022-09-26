3dc29d0b08341632a181139b357b62b182217ba4
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-09-26 15:57:16 -0700

    tuple typing example

---
+++ Types/tuples.ts
@@ -0,0 +1,5 @@
+// tuples are arrays that have a fixed length and fixed order of types.
+
+//for example, maybe you want to set a bunch of colours which should consist of their RGB values
+
+let Colour: [number, number, number] = [12, 34, 234];
