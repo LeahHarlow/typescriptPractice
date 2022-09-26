f77be2585536e6ecabfa55e9b6dc3bbf5a13acde
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-09-26 16:07:05 -0700

    more examples

--- Types/tuples.ts
+++ Types/tuples.ts
@@ -3,3 +3,9 @@
 //for example, maybe you want to set a bunch of colours which should consist of their RGB values
 
 let Colour: [number, number, number] = [12, 34, 234];
+
+// or
+
+type HTTPResponse = [number, string];
+
+let responses: HTTPResponse[] = [[200, "ok"], [500, "sever error"]]
