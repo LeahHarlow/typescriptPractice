b89ef93eadc365802f63fcb5c142034cd7cef1fb
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-09-27 14:20:56 -0700

    enum intro

--- Types/tuplesEnums.ts
+++ Types/tuplesEnums.ts
@@ -1,5 +1,7 @@
 // tuples are arrays that have a fixed length and fixed order of types.
 
+import { isYieldExpression } from 'typescript';
+
 //for example, maybe you want to set a bunch of colours which should consist of their RGB values
 
 let Colour: [number, number, number] = [12, 34, 234];
@@ -11,3 +13,19 @@ type HTTPResponse = [number, string];
 let responses: HTTPResponse[] = [[200, "ok"], [500, "sever error"]]
 
 // anyay thats pretty much the gist and you probably wont use these often
+
+//Enums are a set of named constants for when you want a value to have a limited set of options,
+// Ie maybe you want a shipping status to be Processing, Shipped or Delivered.
+//Enums can be numbers, strings, or heterogenous
+
+enum Answers {
+  yes, //0
+  no, //1
+  maybe //2
+}
+
+const reply = Answers.yes;
+
+// so this all works out and is really nice to read
+
+
